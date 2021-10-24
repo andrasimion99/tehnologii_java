93c2ec25ba115b26ba4e9f4270fd4e53e3f1cd12
Simion, Andra <Andra.Simion@centric.eu>
2021-10-24 08:03:56 +0300

    clean up code

--- lab3/src/main/java/com/lab3/lab3/controller/LanguageController.java
+++ lab3/src/main/java/com/lab3/lab3/controller/LanguageController.java
@@ -1,6 +1,5 @@
 package com.lab3.lab3.controller;
 
-import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
@@ -10,33 +9,21 @@ import java.util.Map;
 import java.util.Locale;
 
 @ManagedBean(name = "languageController", eager = true)
-@ApplicationScoped
+@SessionScoped
 public class LanguageController implements Serializable {
-    private String language;
-    private static Map<String,Object> countries;
+    private static final Map<String,Object> countries;
     static {
 
-        countries = new LinkedHashMap<String,Object>();
+        countries = new LinkedHashMap<>();
         countries.put("English", Locale.ENGLISH);
         countries.put("Italian", Locale.ITALIAN);
     }
 
     public LanguageController() {
-        System.out.println("CONSTRUCTOR");
-    }
-
-    public String getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
     }
 
     public void changeLanguage(String lang){
-        System.out.println("HEREE" + lang);
         for (Map.Entry<String, Object> entry : countries.entrySet()) {
-            System.out.println("Key:" + entry.getValue());
             if(entry.getValue().toString().equals(lang)) {
                 FacesContext.getCurrentInstance().getViewRoot().setLocale((Locale)entry.getValue());
             }
